d2725d06fa7c0354538d751b312b7067533ab782
Guilherme Gonçalves <guilhermegso1019@gmail.com>
2020-12-03 08:51:46 -0300

    setup deploy

--- src/modules/users/providers/HashProvider/fakes/FakeHashProvider.ts
+++
@@ -1,7 +0,0 @@
-import { IHashProvider } from '../models/IHashProvider';
-
-export class FakeHashProvider implements IHashProvider {
-  public async generateHash(payload: string): Promise<string> {
-    return payload;
-  }
-}
